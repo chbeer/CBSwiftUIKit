2ba246f664ab12a9c3ba8142876be593721e03d2
Christian Beer <christian.beer@chbeer.de>
2023-10-11 06:50:00 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CBSwiftUIKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CBSwiftUIKit",
+            targets: ["CBSwiftUIKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "CBSwiftUIKit"),
+        .testTarget(
+            name: "CBSwiftUIKitTests",
+            dependencies: ["CBSwiftUIKit"]),
+    ]
+)
